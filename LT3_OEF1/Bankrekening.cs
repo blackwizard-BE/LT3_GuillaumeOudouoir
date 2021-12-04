21723eea03cbe778905033dee1e0e9479e4c82ce
BlackWizard-BE <guillaume.oudouoir@gmail.com>
2021-12-04 16:23:15 +0100

    oef1 done

---
+++ LT3_OEF1/Bankrekening.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LT3_OEF1
+{
+    internal class Bankrekening
+    {
+
+        public string Name { get; set; }
+        public string Adres { get; set; }
+        public int Postcode { get; set; }
+        public string Woonplaats { get; set; }
+        public double Saldo { get; set; }
+        public string RekeningNummer { get; set; }
+
+
+        public Bankrekening()
+        {
+            
+        }
+        public double Storting(double geld)
+        {
+            Saldo +=geld;
+
+            return Saldo;
+
+
+        }
+        public double Ophaling(double geld)
+        {
+            Saldo -= geld;
+
+            return Saldo;
+
+
+        }
+        
+    }
+}
--- LT3_OEF1/MainWindow.xaml.cs
+++ LT3_OEF1/MainWindow.xaml.cs
@@ -20,9 +20,177 @@ namespace LT3_OEF1
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        int maxPerDag = 7;
+        Bankrekening[] bankrekening = new Bankrekening[7];
+        int rekeningnummercount = 0;
         public MainWindow()
         {
             InitializeComponent();
+
+
+        }
+        public string GenereerRekeningnummer()
+        {
+            string rekeningnummerbegin;
+            int rekeningnummer;
+            Random rnd = new Random();
+            if (rnd.Next(0, 2) == 0)
+            {
+                rekeningnummerbegin = "447";
+            }
+            else
+            {
+                rekeningnummerbegin = "091";
+            }
+            rekeningnummer = Convert.ToInt32($"{rnd.Next(0, 10)}{rnd.Next(0, 10)}{rnd.Next(0, 10)}{rnd.Next(0, 10)}{rnd.Next(0, 10)}{rnd.Next(0, 10)}{rnd.Next(0, 10)}");
+            double controle = Convert.ToDouble($"{rekeningnummerbegin}{rekeningnummer}");
+            controle = controle % 97;
+            if (controle == 0)
+            {
+                controle = 97;
+            }
+            return $"{rekeningnummerbegin}-{rekeningnummer}-{controle}";
+        }
+
+        private void btnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                
+                bankrekening[rekeningnummercount] = new Bankrekening();
+                bankrekening[rekeningnummercount].Name = txbNaam.Text;
+                bankrekening[rekeningnummercount].Adres = txbAdres.Text;
+                bankrekening[rekeningnummercount].Postcode = Convert.ToInt32(txbPostcode.Text);
+                bankrekening[rekeningnummercount].Woonplaats = txbWoonplaats.Text;
+                bankrekening[rekeningnummercount].Saldo = 0;
+                bankrekening[rekeningnummercount].RekeningNummer = GenereerRekeningnummer();
+                lsbBankrekeningen.Items.Add($"{rekeningnummercount + 1}:        {bankrekening[rekeningnummercount].RekeningNummer}");
+                cmbVan.Items.Add(bankrekening[rekeningnummercount].RekeningNummer);
+                cmbNaar.Items.Add(bankrekening[rekeningnummercount].RekeningNummer);
+                rekeningnummercount++;
+                txbNaam.Clear();
+                txbAdres.Clear();
+                txbWoonplaats.Clear();
+                txbPostcode.Clear();
+                if (rekeningnummercount + 1 == (maxPerDag))
+                {
+                    MessageBox.Show("je hebt je maximum bereikt.");
+                    btnAdd.IsEnabled = false;
+                }
+                labelUpdate();
+            }
+            catch (Exception error)
+            {
+                MessageBox.Show($"geef juiste waarden in aub");
+            }
+        }
+
+
+
+        private void lsbBankrekeningen_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (lsbBankrekeningen.SelectedIndex != -1)
+            {
+                int index = lsbBankrekeningen.SelectedIndex;
+                txbNaam.Text = bankrekening[index].Name;
+                txbAdres.Text = bankrekening[index].Adres;
+                txbPostcode.Text = bankrekening[index].Postcode.ToString();
+                txbWoonplaats.Text = bankrekening[index].Woonplaats;
+                btnStorting.IsEnabled = true;
+                btnOphaling.IsEnabled = true;
+                btnOverschrijven.IsEnabled = true;
+            }
+        }
+
+        private void btnStorting_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (lsbBankrekeningen.SelectedIndex != -1)
+                {
+                    int index = lsbBankrekeningen.SelectedIndex;
+                    MessageBox.Show($"Storting successvol nieuwe bedrag ${bankrekening[index].Storting(Convert.ToDouble(txbBedrag.Text))}");
+                    txbBedrag.Clear();
+                }
+            }
+            catch
+            {
+                if (txbBedrag.Text != null)
+                {
+                    MessageBox.Show("Je hebt een verkeerde waarde ingegeven");
+                }
+                else
+                {
+                    MessageBox.Show("Je hebt geen waarde ingegeven");
+                }
+            }
+            labelUpdate();
+        }
+
+        private void btnOphaling_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (lsbBankrekeningen.SelectedIndex != -1)
+                {
+                    int index = lsbBankrekeningen.SelectedIndex;
+                    MessageBox.Show($"Storting successvol nieuwe bedrag ${bankrekening[index].Ophaling(Convert.ToDouble(txbBedrag.Text))}");
+                    txbBedrag.Clear();
+                }
+            }
+            catch
+            {
+                if(txbBedrag.Text != null)
+                {
+                    MessageBox.Show("Je hebt een verkeerde waarde ingegeven");
+                }
+                else
+                {
+                    MessageBox.Show("Je hebt geen waarde ingegeven");
+                }
+            }
+            labelUpdate();
+        }
+
+        private void btnOverschrijven_Click(object sender, RoutedEventArgs e)
+        {
+            bankrekening[rekeningSearch(cmbVan.Text)].Ophaling(Convert.ToDouble(txbBedrag.Text));
+            bankrekening[rekeningSearch(cmbNaar.Text)].Storting(Convert.ToDouble(txbBedrag.Text));
+            labelUpdate();
+            txbBedrag.Clear();
+
+
+        }
+        public int rekeningSearch(string cmbText)
+        {
+            bool search = true;
+            int i = 0;
+            do
+            {
+                if (cmbText == bankrekening[i].RekeningNummer)
+                {
+                    search = false;
+                }
+                else
+                {
+                    i++;
+                }
+
+
+
+            }
+            while (search);
+            return i;
+        }
+        public void labelUpdate()
+        {
+            int imax = lsbBankrekeningen.Items.Count;
+            lblSaldo.Content = "";
+            for (int i = 0; i < imax; i++)
+            {
+                lblSaldo.Content = $"{lblSaldo.Content}\nSaldo rekening {i+1}= $ {Math.Round(bankrekening[i].Saldo,2)}";
+            }
         }
     }
 
